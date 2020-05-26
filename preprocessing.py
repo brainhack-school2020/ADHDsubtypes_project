c90597b12a07de445db532150fe5a56977ba3589
beapdk <45300124+beapdk@users.noreply.github.com>
2020-05-26 17:39:58 -0400

    changes to function (reduce df)

--- preprocessing.py
+++ preprocessing.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import glob
+import os
 
 def process_all_excel_files():
     df_full = pd.DataFrame()
@@ -13,13 +14,13 @@ def process_all_excel_files():
 def process_excel_file(path):
     df = pd.read_excel(path, #file
                    header=None,
-                   usecols="A:O") #selecting columns of interest
+                   usecols="A:E") #selecting columns of interest
     brain_oscil = df.iloc[63].values
     freq_bands = df.iloc[64].values
 
-    df = df.iloc[63:86] # selecting rows of interest
+    df = df.iloc[63:84] # selecting rows of interest
     col_names = {} # create dictionary for column names 
-    for i in range(14+1):
+    for i in range(4+1):
         if i == 0:
             col_name = 'electrode'
         else:
@@ -39,6 +40,6 @@ def process_excel_file(path):
     df = pd.melt(df, id_vars="electrode", value_vars=df_col, var_name="brain_oscillation", value_name='fft_abs_power')
     df['freq_band'] = df['brain_oscillation'].apply(lambda x: x.split('_')[1])#split into two columns
     df['brain_oscillation'] = df['brain_oscillation'].apply(lambda x: x.split('_')[0])
-    df["id"]=path.split("/")[1].split(".")[0] #WARNING!!! IF USING WINDOWS, CHANGE "/" TO "\\". No additionnal changes are needed.
+    df["id"]=path.split("/")[1].split(".")[0]
     df["electrode"]= df["electrode"].apply(lambda x: x.split('-')[0])#splitting for not a string
     return df
