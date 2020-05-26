3d83288a1a3a430d151f67720db788bf8ec96e98
penelopepg <65008048+penelopepg@users.noreply.github.com>
2020-05-26 14:14:07 -0400

    Line 42 switch "/" to "\\" if on windows
    Added a comment on line 42 instructing users to make a minor modification to the script if using windows.

--- preprocessing.py
+++ preprocessing.py
@@ -39,6 +39,6 @@ def process_excel_file(path):
     df = pd.melt(df, id_vars="electrode", value_vars=df_col, var_name="brain_oscillation", value_name='fft_abs_power')
     df['freq_band'] = df['brain_oscillation'].apply(lambda x: x.split('_')[1])#split into two columns
     df['brain_oscillation'] = df['brain_oscillation'].apply(lambda x: x.split('_')[0])
-    df["id"]=path.split("/")[1].split(".")[0]
+    df["id"]=path.split("/")[1].split(".")[0] #WARNING!!! IF USING WINDOWS, CHANGE "/" TO "\\". No additionnal changes are needed.
     df["electrode"]= df["electrode"].apply(lambda x: x.split('-')[0])#splitting for not a string
     return df
