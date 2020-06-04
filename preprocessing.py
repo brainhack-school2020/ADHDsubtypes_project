5d06d7e6575d4066a3ef435e992846eb7ddd16c0
beapdk <beatrice.pelletier-de.koninck@umontreal.ca>
2020-06-03 22:33:27 -0400

    added pool function

--- preprocessing.py
+++ preprocessing.py
@@ -14,13 +14,13 @@ def process_all_excel_files():
 def process_excel_file(path):
     df = pd.read_excel(path, #file
                    header=None,
-                   usecols="A:E") #selecting columns of interest
+                   usecols="A:L") #selecting columns of interest
     brain_oscil = df.iloc[63].values
     freq_bands = df.iloc[64].values
 
     df = df.iloc[63:84] # selecting rows of interest
     col_names = {} # create dictionary for column names 
-    for i in range(4+1):
+    for i in range(11+1):
         if i == 0:
             col_name = 'electrode'
         else:
@@ -52,4 +52,18 @@ def categorize_subtypes(inat, hyper, std_dev=5.72):# clinical std_dev as thresho
             return 'inat'
         else:
             return 'hyper'
+
+def electrode_pools(electrode):
+    if electrode in ['FP2', 'FP1', 'Fz', 'F3', 'F4', 'F7', 'F8']:
+        return 'frontal'
+    if electrode in ['C3','C4','Cz']:
+        return 'central'
+    if electrode in ['T3', 'T4', 'T5', 'T6']:
+        return 'temporal'
+    if electrode in ['P3', 'P4', 'Pz']:
+        return 'parietal'
+    if electrode in ['O1', 'O2']:
+        return 'occipital'
+    else:
+        return 'N/A'
     
